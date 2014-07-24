35b42f33262f29ebf075cd70f575ef09a67c8930
monkeyde17 <monkey_tv@126.com>
2014-07-25 07:29:49 +0800

    1. first line 2. second line 3. end line =========== final, by etond

--- a.cpp
+++ a.cpp
@@ -8,5 +8,10 @@ int main(int argc, char *argv[])
 	// this is a commond
 	cin << a << b;
 	cout << a + b << endl;
+
+	for (int i = 0; i < 10; i++)
+	{
+		cout << "look out" << endl;
+	}
 	return 0;
 }
