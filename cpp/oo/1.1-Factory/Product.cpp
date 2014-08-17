38b98548b4ecbd1ac5d09911dc1ddd4ece465717
monkeyde17 <monkey_tv@126.com>
2014-08-17 16:11:07 +0800

    add Factory mode, oo

---
+++ cpp/oo/1.1-Factory/Product.cpp
@@ -0,0 +1,25 @@
+#include "Product.h"
+
+#include <iostream>
+
+using namespace std;
+
+Product::Product()
+{
+
+}
+
+Product::~Product()
+{
+
+}
+
+ConcreteProduct::ConcreteProduct()
+{
+	cout << " ConcreteProduct ..." << endl;
+}
+
+ConcreteProduct::~ConcreteProduct()
+{
+
+}
---
+++ cpp/oo/1.1-Factory/run.bat
@@ -0,0 +1,7 @@
+g++ Factory.cpp main.cpp Product.cpp -o main.exe
+
+pause
+
+main.exe
+
+pause
