f87ec559e6de8bca2295c641f8ea614ef8f0b249
Duan <monkey_tv@126.com>
2014-08-30 00:19:59 +0800

    modify run.sh

--- cpp/oo/1.4-Builder/run.sh
+++ cpp/oo/1.4-Builder/run.sh
@@ -1,9 +1,3 @@
-#########################################################################
-# File Name: run.sh
-# Author: etond
-# mail: monkey_tv@126.com
-# Created Time: 五  8/29 23:45:28 2014
-#########################################################################
 #!/bin/bash
 g++ main.cpp Director.cpp Builder.cpp Product.cpp -o main.out
 ./main.out
